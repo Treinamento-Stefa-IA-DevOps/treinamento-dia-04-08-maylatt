b8c0398e0509bf1a4d33dd11c8c87b16e937a90e
Mayla Taiane Teixeira <maylateixeira2013@gmail.com>
2021-08-06 16:52:14 -0300

    [TASK] Implements Endpoint for prediction

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,13 +1,26 @@
 import pickle
 from fastapi import FastAPI
+import pandas as pd
+import os
 
 app = FastAPI()
 
 @app.post('/model')
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
-    with open('model/Titanic.pkl', 'rb') as fid: 
+def titanic(Sex:int, Age: float, Lifeboat: int, Pclass: int):
+    with open(f'{os.path.dirname(__file__)}/model/Titanic.pkl', 'rb') as fid: 
         titanic = pickle.load(fid)
+        ## Cria o dataframe para enviar para o modelo (ordena os parametros alfabeticamente)
+        df = pd.DataFrame([[Age, Lifeboat, Pclass, Sex]], columns =['Age', 'Lifeboat', 'Pclass', 'Sex'])
+        ## Realiza a predição (TODO: tratar exceções)
+        y = titanic.predict(df)
+        survived = bool(y[0])
+        response = {
+            "survived": survived,
+            "status": 200,	
+            "message": "Sobreviveu!" if survived else "Não sobreviveu!",	
+        }
+        return response
 
 @app.get('/model')
 def get():
